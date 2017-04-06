96175cabe456cab38e3da7b145ea67d433a4eacc
Евгений <Yauheni.r@mail.ru>
2017-04-06 23:06:55 +0100

    Initial commit

---
+++ src/main/java/ru/yauheni/pam_parser/parser/main.java
@@ -0,0 +1,14 @@
+package ru.yauheni.pam_parser.parser;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+
+/**
+ * Created by Евгений on 06.04.2017.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Workbook wb = new HSSFWorkbook();
+    }
+}
